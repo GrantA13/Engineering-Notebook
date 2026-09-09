455d0b3b5f33b31ff6e3cde797d8eb559d56d7a1
GrantA13 <99507465+GrantA13@users.noreply.github.com>
2026-09-09 10:07:03 -0400

    Initial Commit

---
+++ Engineering Notebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Engineering Notebook
+//
+//  Created by Grant Andrews on 9/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Engineering Notebook/Engineering_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Engineering_NotebookApp.swift
+//  Engineering Notebook
+//
+//  Created by Grant Andrews on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Engineering_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
